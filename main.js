94762335ccac140f0616dc0602632f90bd674aff
Evan Kalvis <evankalvis@hotmail.com>
2017-01-15 03:28:34 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,55 @@
+var backgrounds = [{
+  image: "http://www.hdbloggers.net/wp-content/uploads/2016/01/HD-Desktop-Background.jpg"
+}, {
+  image: "http://awesomewallpapers.files.wordpress.com/2010/01/zcity-7.jpg"
+}, {
+  image: "http://www.emoticonswallpapers.com/wallpaper/cities/cities-wallpaper-005.jpg"
+}, {
+  image: "http://www.desktopwallpaperhd.net/wallpapers/17/d/night-city-background-computer-179261.jpg"
+}, {
+  image: "http://wallpaperrs.com/uploads/photography/one-world-trade-center-cities-spectacular-hd-wallpaper-142944107626.jpg"
+}, {
+  image: "https://wallpaperscraft.com/image/new_york_city_top_view_85115_1920x1080.jpg"
+}, {
+  image: "http://cdn.wallpapersafari.com/42/40/wNqXGR.jpg"
+}, {
+  image: "http://travelhdwallpapers.com/wp-content/uploads/2015/01/Sydney-Night-8.jpg"
+}, {
+  image: "http://cdn.wallpapersafari.com/59/27/oNU5fY.jpg"
+}, {
+  image: "http://cdn.pcwallart.com/images/germany-city-at-night-wallpaper-2.jpg"
+}]
+
+function randomBackground() {
+  var random = Math.floor(Math.random() * backgrounds.length);
+  if (random == 0) {
+    random = 1;
+  }
+
+  document.getElementsByTagName("body")[0].style.backgroundImage = "url(" + backgrounds[random].image + ")";
+
+}
+
+document.getElementById("new-quote").onclick = fwork;
+
+var tweetQuote = '';
+function fwork() {
+  randomBackground();
+  $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?", function(data) {
+    $("#actual-quote").html('"' + data.quoteText + '"')
+    $("#author").html("<p>&ndash;" + data.quoteAuthor + "<p>")
+
+  if (data.quoteAuthor) {
+  $("#author").html("<p>&ndash;" + data.quoteAuthor + "</p>")
+} else {
+  $("#author").html("<p>&ndash;"+"Unknown!"+ "</p>")
+}
+    tweetQuote=data.quoteText+'-'+""+data.quoteAuthor;
+  });
+
+};
+
+$("#twitter").click(function(){
+  console.log(tweetQuote);
+    window.open('https://twitter.com/intent/tweet?text='+tweetQuote,'_blank');
+  });
